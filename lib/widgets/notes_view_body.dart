88b7a72cf59e34297cbcbdedc15cbf6cee7848ef
AbdallaSalah200 <abdallasalah3333@gmail.com>
2023-08-22 23:50:28 +0300

    create notes view body

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,20 @@
+import 'custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:16 ),
+      child: Column(
+        children: const[
+          SizedBox(height: 45,),
+     CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
