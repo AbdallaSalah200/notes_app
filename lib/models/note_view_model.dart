756962dbeedc6d73688cf32589f5ea53bf9aed2f
AbdallaSalah200 <abdallasalah3333@gmail.com>
2023-08-24 07:07:54 +0300

    createnoteviewmodel

---
+++ lib/models/note_view_model.dart
@@ -0,0 +1,9 @@
+class NoteViewModel{
+  final String title ;
+  final String subtitle;
+  final int color ;
+  final String date ;
+
+  NoteViewModel({required this.title, required  this.subtitle,required   this.color, required  this.date});
+
+}
