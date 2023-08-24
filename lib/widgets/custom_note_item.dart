23bff14340e6cf321308c1dea0812f3cc6cd6683
AbdallaSalah200 <abdallasalah3333@gmail.com>
2023-08-24 06:39:55 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NoteItem extends StatelessWidget {
@@ -6,30 +7,35 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color:  Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-        
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-      ListTile(
-        title: Text("Flutter tips",style: TextStyle(color:Colors.black,fontSize: 28),),
-        subtitle:  Padding(
-          padding: const EdgeInsets.only(top: 9),
-          child: Text("Build your carrer with thrawet samy",style: TextStyle(color:Colors.black.withOpacity(.5),fontSize: 20),),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){return  EditNoteView();}));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
+        decoration: BoxDecoration(
+          color: const  Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+          
         ),
-        trailing:IconButton (onPressed:(){},icon: Icon(FontAwesomeIcons.trash,color: Colors.black,size:24), ) ,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+        ListTile(
+          title:const  Text("Flutter tips",style: TextStyle(color:Colors.black,fontSize: 28),),
+          subtitle:  Padding(
+            padding: const EdgeInsets.only(top: 9),
+            child: Text("Build your carrer with thrawet samy",style: TextStyle(color:Colors.black.withOpacity(.5),fontSize: 20),),
+          ),
+          trailing:IconButton (onPressed:(){},icon:const  Icon(FontAwesomeIcons.trash,color: Colors.black,size:24), ) ,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Text("23Aug 2023",style: TextStyle(color: Colors.black.withOpacity(.4)),),
+        ),
+          ],
+        ) 
       ),
-      Padding(
-        padding: const EdgeInsets.only(right: 16),
-        child: Text("23Aug 2023",style: TextStyle(color: Colors.black.withOpacity(.4)),),
-      ),
-        ],
-      ) 
     );
   }
 }
